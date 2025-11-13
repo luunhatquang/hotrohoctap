8a5ffa960354f44e613033c2f92939c4e2a70118
nhatngoonslaof <luunhat172@gmail.com>
2025-11-14 01:28:32 +0700

    Fix ALLOWED_HOSTS for Render

--- searchuniversity/searchuniversity/settings.py
+++ searchuniversity/searchuniversity/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default="django-insecure-pj#yod)5d^pw85k(9=_mp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.vercel.app,.now.sh').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.vercel.app,.now.sh,.onrender.com').split(',')
 
 
 # Application definition
