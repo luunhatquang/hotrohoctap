9c9fd607835dbb379415b1d35d67293e04a2e8de
nhatngoonslaof <luunhat172@gmail.com>
2025-11-14 01:05:05 +0700

    Fix Vercel 404 error - update config

--- searchuniversity/wsgi.py
+++ searchuniversity/wsgi.py
@@ -1,7 +1,17 @@
 """
-WSGI config wrapper for Vercel deployment
+WSGI config for Vercel deployment
 """
-from searchuniversity.wsgi import application
+import os
+import sys
+
+# Add project directory to path
+sys.path.append(os.path.dirname(__file__))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searchuniversity.settings')
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
 
 # Vercel serverless function handler
 app = application
